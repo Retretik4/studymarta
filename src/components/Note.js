8e7e3edd5c77dd3dde25cebfdbd5165988301d0b
TeoDor <retretik4@gmail.com>
2023-01-20 15:08:39 +0300

    Start point

---
+++ src/components/Note.js
@@ -0,0 +1,17 @@
+const Note = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>Ячейка 1</td>
+        <td>Ячейка 2</td>
+        <td>Ячейка 3</td>
+        <td>Ячейка 4</td>
+        <td>Ячейка 5</td>
+        <td>Ячейка 6</td>
+        <td>Ячейка 7</td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default Note;
